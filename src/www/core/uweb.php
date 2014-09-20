f14ccce45514e89f775949d7b3697dc1b2857fd8
Pedro A. Hortas <pah@ucodev.org>
2014-09-20 23:12:23 +0100

    v0.00-skeleton-04

---
+++ src/www/core/uweb.php
@@ -0,0 +1,31 @@
+<?php
+
+class UW_Config {
+	private $_use_session = false;
+	private $_use_cookies_encrypted = false;
+
+	public function set_session($state = false) {
+		$this->_use_session = $state;
+	}
+
+	public function get_session() {
+		return $this->_use_session;
+	}
+
+	public function set_cookies_encrypted($state = false) {
+		$this->_use_cookies_encrypted = $state;
+	}
+
+	public function get_cookies_encrypted() {
+		return $this->_use_cookies_encrypted;
+	}
+}
+
+class UW_Core {
+	public $config = null;
+
+	public function __construct() {
+		$this->config = new UW_Config;
+	}
+}
+
